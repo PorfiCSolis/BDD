9ed6b474965fc0b312dac781923ad7bc604c3f3e
Porfirio <User@DESKTOP-DC86VHC>
2021-11-08 21:00:37 -0600

    Cambios de prueba

--- BDD/Form1.Designer.cs
+++ BDD/Form1.Designer.cs
@@ -28,73 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.l1 = new System.Windows.Forms.Label();
-            this.l2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(145, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(47, 50);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // l1
-            // 
-            this.l1.AutoSize = true;
-            this.l1.Location = new System.Drawing.Point(111, 111);
-            this.l1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.l1.Name = "l1";
-            this.l1.Size = new System.Drawing.Size(55, 13);
-            this.l1.TabIndex = 2;
-            this.l1.Text = "Hola pops";
-            this.l1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // l2
-            // 
-            this.l2.AutoSize = true;
-            this.l2.Location = new System.Drawing.Point(89, 133);
-            this.l2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.l2.Name = "l2";
-            this.l2.Size = new System.Drawing.Size(98, 13);
-            this.l2.TabIndex = 3;
-            this.l2.Text = "Saca la mota rey c:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.l2);
-            this.Controls.Add(this.l1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label l1;
-        private System.Windows.Forms.Label l2;
     }
 }
 
